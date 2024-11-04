ec94134f7e7785b6455d9d9243e872303fbebae4
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-11-04 16:30:18 +0530

    section one complete

--- src/components/landing/Setions/bgSection/bgSection.jsx
+++ src/components/landing/Setions/bgSection/bgSection.jsx
@@ -1,9 +1,8 @@
 
-import React from "react";
 import Regbtn from '../../../core/button/index';
 import { Container, ContainerWrap, BgContainer } from "../../Style";
 import {Main} from "./style"
-import Data from '../datas'
+import {Data }from '../datas'
 import BG from '../../../../assets/landing/vistaBg.jpg';
 
 const BgSection = () => {
--- src/components/landing/Setions/datas.js
+++ src/components/landing/Setions/datas.js
@@ -1,14 +1,17 @@
 // datas.js
-const Data = {
-  p:'Best psychologist',
-  h1: `    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-              esse nobis reprehenderit aut cumque labore sapiente tempora
-              eveniet veniam quaerat, distinctio minima est voluptatum, error
-              tempore. Minima, totam. Ratione, ullam?`,
-            
+import SectionImg from "../../../assets/landing/about-image-1 (1).jpg";
+
+export const Data = {
+  p: "Best psychologist",
+  h1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, esse nobis reprehenderit aut cumque labore sapiente tempora eveniet veniam quaerat, distinctio minima est voluptatum, error tempore. Minima, totam. Ratione, ullam?",
 };
 
-
-
-
-export default Data;
+export const SectionOne = {
+  img: SectionImg,
+  h5: "Let me introduce",
+  h1: "I’m Dr. Susan Lopez – Expert Psychologist from New York.",
+  h1Break: "Psychologist from New York.",
+  h3: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua minim veniam.",
+  p: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Diam nascetur ad varius curabitur ante donec de proin auctor felis hendrerit.",
+  h4: "Susan Lopez – Founder",
+};
